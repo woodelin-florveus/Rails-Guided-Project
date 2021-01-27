9413402752258b2221bc3b47024c2ec091cadd8a
woodelin-florveus <woodelinflorveus@gmail.com>
2021-01-26 20:54:27 -0500

    users feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,23 @@ class UsersController < ApplicationController
   end
 
   def create
-    new_user = User.create(username: params[:username], email: params[:email], password: params[:password])
-    redirect_to "/users/#{new_user.id}"
+    # new_user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    # redirect_to "/users/#{new_user.id}"
+
+    @user = User.create(user_params(:username, :email, :password))
+    redirect_to user_path(@user.id)
   end
 
   def show
     # byebug
     @user = User.find(params[:id])
     @picture = User.find(params[:id])
+  end
+
+  private
+
+  def user_params(*args)
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
